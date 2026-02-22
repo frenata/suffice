cea52da85d683dd3adcfa94f7a93397b0ff4b0d3
Andrew Nichols <andrew@frenata.net>
2026-02-21 22:40:56 -0500

    split lib and bin crates

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter, bleuuid::uuid_from_u16};
-use btleplug::platform::{Adapter, Manager, Peripheral};
+use btleplug::api::Peripheral as _;
 use std::env;
 use std::error::Error;
-use std::time::Duration;
-use tokio::time;
-use uuid::Uuid;
+
+use suffice::trainer::Trainer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let target = args[1].clone();
     // find the device we're interested in
-    if let Some(trainer) = find_trainer(target.clone()).await {
+    if let Some(trainer) = Trainer::find(target.clone()).await {
         eprint!("{:?}", trainer);
-        trainer.connect().await?;
-        trainer.discover_services().await?;
-        for c in trainer.characteristics() {
+        trainer.peri.connect().await?;
+        trainer.peri.discover_services().await?;
+        for c in trainer.peri.characteristics() {
             eprintln!("{:?}", c)
         }
     } else {
@@ -23,30 +21,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
         return Err(format!("{} not found", target).into());
     }
     Ok(())
-}
-
-async fn find_trainer(target: String) -> Option<Peripheral> {
-    let manager = Manager::new().await.unwrap();
-
-    // get the first bluetooth adapter
-    let adapters = manager.adapters().await.ok()?;
-    let central = adapters.into_iter().nth(0).unwrap();
-
-    // start scanning for devices
-    central.start_scan(ScanFilter::default()).await.ok()?;
-    // instead of waiting, you can use central.events() to get a stream which will
-    // notify you of new devices, for an example of that see examples/event_driven_discovery.rs
-    time::sleep(Duration::from_secs(2)).await;
-    for p in central.peripherals().await.ok()? {
-        if let Some(props) = p.properties().await.ok()? {
-            // eprintln!("{:?}", props.local_name);
-            if let Some(name) = props.local_name
-                && name == target
-            {
-                return Some(p);
-            }
-        }
-    }
-
-    None
 }
---
+++ src/trainer.rs
@@ -0,0 +1,38 @@
+use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter, bleuuid::uuid_from_u16};
+use btleplug::platform::{Manager, Peripheral};
+use std::time::Duration;
+use tokio::time;
+use uuid::Uuid;
+
+#[derive(Debug)]
+pub struct Trainer {
+    pub peri: Peripheral,
+}
+
+impl Trainer {
+    pub async fn find(target: String) -> Option<Trainer> {
+        let manager = Manager::new().await.unwrap();
+
+        // get the first bluetooth adapter
+        let adapters = manager.adapters().await.ok()?;
+        let central = adapters.into_iter().nth(0).unwrap();
+
+        // start scanning for devices
+        central.start_scan(ScanFilter::default()).await.ok()?;
+        // instead of waiting, you can use central.events() to get a stream which will
+        // notify you of new devices, for an example of that see examples/event_driven_discovery.rs
+        time::sleep(Duration::from_secs(2)).await;
+        for p in central.peripherals().await.ok()? {
+            if let Some(props) = p.properties().await.ok()? {
+                // eprintln!("{:?}", props.local_name);
+                if let Some(name) = props.local_name
+                    && name == target
+                {
+                    return Some(Trainer { peri: p });
+                }
+            }
+        }
+
+        None
+    }
+}
